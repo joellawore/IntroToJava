a3e543caa6a0954b045845bc77834bc14c54d6a3
joell <joell@DESKTOP-787610E>
2020-09-22 01:10:17 -0400

    first push

---
+++ IntroToJava/src/firstFile.java
@@ -0,0 +1,55 @@
+// joel's file
+public class firstFile {
+	public static void main(String args[]){
+
+		System.out.println("Who are you?");
+		
+		System.out.println();
+		
+		System.out.println("I am Joel Lawore, and I am a student at St. Mark's School.");
+		
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		System.out.println("Where are you from?");
+		
+		System.out.println();
+		
+		System.out.println("I'm from Colonia, New Jersey, and I've lived there all my life.");
+		
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		System.out.println("What is your computer science background?");
+		
+		System.out.println();
+		
+		System.out.println("I've always had a fondess for computers, whether it was typing or playing video games, and I was exposed to the idea of computer science at a fairly young age. When I reached high school and I began to tap into this interest a lot more, and ever since I've been getting better and better at it every day. I've now done a few programs including a Microsoft Intership which including a hackathon, which I won. ");
+		
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		System.out.println("What are some of your passions?");
+		
+		System.out.println();
+		
+		System.out.println("I'm a very huge football/soccer fan, I support Arsenal. In addition I love music and singing, I'm actually a member of the school's acapella group, and I enjoy wrestling as well.");
+		
+		System.out.println();
+		System.out.println();
+		
+
+		System.out.println("What are you looking forward to in this class?");
+		
+		System.out.println();
+		
+		System.out.println("I'm hoping to be able to learn how to apply my knowledge of computer science more effectively while also expanding on that knowledge.");
+		
+	}
+}
