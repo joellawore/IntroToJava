502a4aa3b1a8abddc48017bfbbcdb726d7a7046d
joell <joell@DESKTOP-787610E>
2020-09-22 11:43:22 -0400

    SumIt assignment

---
+++ IntroToJava/src/SumIt.java
@@ -0,0 +1,40 @@
+public class SumIt {
+	
+  // instance variables
+  private double one, two, sum;
+  
+  // use the two parameters to set the values of your two instance variables
+  public void setNums(double num1, double num2) {
+	  one = num1;
+	  two = num2;
+	  // your code here
+  }
+  
+  // sum the two numbers here
+  public void sum() {
+	  // your code here
+	  sum = one + two;
+  }
+  
+  // print the sum here
+  public void print() {
+	  // your code here
+	  System.out.println(sum);
+  }
+  
+  
+  public static void main(String[] args) {
+	  SumIt runner = new SumIt();
+	  
+	  // should print out 12
+	  runner.setNums(5,  7);
+	  runner.sum();
+	  runner.print();
+	  
+	  // should print out 40
+	  runner.setNums(13, 27);
+	  runner.sum();
+	  runner.print();
+  }
+  
+}
