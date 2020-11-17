b5087e9f446ed8b1c05d47d9ec02f23c2b35d358
joell <joell@10.1.98.246>
2020-11-17 11:54:18 -0500

    Pong

--- IntroToJava/src/Pong.java
+++ IntroToJava/src/Pong.java
@@ -26,16 +26,26 @@ public class Pong extends JPanel implements KeyListener {
 	// your instance variables here, I've given you a few 
 	private boolean up1, down1, up2, down2; 		// booleans to keep track of paddle movement
 	private boolean multiplayerCheck = true;
+	private boolean pause = false;
+	private boolean stun1 = false, stun2 = false;
+	private boolean star1 = false, star2 = false, rocket1 = false, rocket2 = false, rl1 = false, rl2 = false;
+	private int ra1 = 0, ra2 = 0;
+	private boolean r = false;
 	private int xball = WIDTH/2, yball = HEIGHT/2;
 	private int ypaddle1 = HEIGHT/2;
 	private int ypaddle2 = HEIGHT/2;
+	private int rockx1 = 0, rocky1 = HEIGHT/2;
+	private int rockx2 = 0, rocky2 = HEIGHT/2;
+	private int rockx2l = XPADDLE2, rocky2l = ypaddle2;
 	private int xvelo = 10;
-	private int yvelo = 7;
-	private int pspeed = 8;
+	private int yvelo = 6;
+	private int pspeed = 5;
 	private int paddle_speed1 = 0;
 	private int paddle_speed2 = 0;
 	private int score1 = 0;
 	private int score2 = 0;
+	private int s = 0;
+	private int time;
 	
 	// this method moves the ball at each timestep
 	public void move_ball() {
@@ -45,28 +55,74 @@ public class Pong extends JPanel implements KeyListener {
 		yball += yvelo;
 	}
 	
+	
+	public void rocket2(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillOval(rockx2, rocky2, 30, 15);
+		g.setColor(Color.GRAY);
+		g.fillRect(rockx2, rocky2, 10, 15);
+		rockx2 +=3;
+	}
+	
+	public void rocket2l(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillOval(rockx2l, rocky2l, 30, 15);
+		g.setColor(Color.GRAY);
+		g.fillRect(rockx2l, rocky2l, 10, 15);
+		rockx2l -=3;
+	}
+	
 	// this method moves the paddles at each timestep
 	public void move_paddles() {
-		if (up1) {
-			paddle_speed1 = 0 - pspeed;
+		if (!stun1) {
+			if (up1) {
+				paddle_speed1 = 0 - pspeed;
+			}
 		}
-		if (up2) {
-			paddle_speed2 = 0 - pspeed;
+		
+		if (multiplayerCheck) {
+			if (!stun2) {
+				if (up2) {
+					paddle_speed2 = 0 - pspeed;
+				}
+			}
 		}
-		if (down1) {
-			paddle_speed1 = pspeed;
+		if (!stun1) {
+			if (down1) {
+				paddle_speed1 = pspeed;
+			}
 		}
-		if (down2) {
-			paddle_speed2 = pspeed;
+		
+		if (multiplayerCheck) {
+			if (!stun2) {
+				if (down2) {
+					paddle_speed2 = pspeed;
+				}
+			}
 		}
+		
 		if (!up1 && !down1) {
 			paddle_speed1 = 0;
 		}
 		if (!up2 && !down2) {
 			paddle_speed2 = 0;
 		}
+		
+		if (!multiplayerCheck) {
+			if (ypaddle2 > yball) {
+				paddle_speed2 = 0 - pspeed;
+			}
+			else if (ypaddle2 <  yball) {
+				paddle_speed2 = pspeed;
+			}
+			else {
+				paddle_speed2 = 0;
+			}
+		}
+		
 		ypaddle1 += paddle_speed1;
 		ypaddle2 += paddle_speed2;
+		
 		
 		// your code here // 
 	}
@@ -75,6 +131,7 @@ public class Pong extends JPanel implements KeyListener {
 	// and if the ball has reached a left/right wall it adds to 
 	// the corresponding player's score
 	public void check_collisions() {
+		// ball to wall collisions
 		if (xball >= WIDTH-DIAM || xball <= 0) {
 			xvelo = -xvelo;
 		}
@@ -88,14 +145,32 @@ public class Pong extends JPanel implements KeyListener {
 			score1 ++;
 		}
 		
-		// your code here
+		// ball to paddle collisions
 		if ((yball >= ypaddle1) && (yball + DIAM <= ypaddle1 + PADDLE_HEIGHT) && (xball <= XPADDLE1 + PADDLE_WIDTH)) {
 			xvelo = 0 - xvelo;
 		} 
 		
 		if ((yball >= ypaddle2) && (yball + DIAM <= ypaddle2 + PADDLE_HEIGHT) && (xball + DIAM >= XPADDLE2)) {
 			xvelo = 0 - xvelo;
-		} 
+		}
+		
+		// rocket to paddle collisions
+		
+	}
+	
+	public void rocket2_collisions() {
+		if ((rocky2 >= ypaddle2) && (rocky2 + DIAM <= ypaddle2 + PADDLE_HEIGHT) && (rockx2 + DIAM >= XPADDLE2)) {
+			rocket2 = true;
+			ra2 = 3;
+		}
+	}
+	public void rocket2l_collisions() {
+		if ((rocky2 >= ypaddle1) && (rocky2 + DIAM <= ypaddle1 + PADDLE_HEIGHT) && (rockx2 <= XPADDLE1)) {
+			rl2 = false;
+			ra2 --;
+			if (ra2 == 0)
+				rocket2 = false;
+		}
 	}
 	
 
@@ -120,7 +195,22 @@ public class Pong extends JPanel implements KeyListener {
 		g.setFont(f);
 		g.setColor(Color.red);
 		g.drawString("P1 Score: " + score1, WIDTH/5, 20);
+		g.drawString("Time: " + time, WIDTH*2/5, 20);
+		g.drawString("R: " + rocket2, WIDTH*4/5, 20);
 		g.drawString("P2 Score: " + score2, WIDTH*3/5, 20);
+		
+		if (pause)
+			g.drawString("Game paused, press 'p' to resume.", WIDTH/2-125, HEIGHT/2);
+		
+		if (time >= 10 && rockx2 <= WIDTH-30) {
+			rocket2(g);
+			rocket2_collisions();
+		}
+		
+		if (rl1 && rockx2l >= XPADDLE1) {
+			rocket2l(g);
+			rocket2l_collisions();
+		}
 	}
 
 	// defines what we want to happen if a keyboard button has 
@@ -130,17 +220,13 @@ public class Pong extends JPanel implements KeyListener {
 		int keyCode = e.getKeyCode();
 		
 		// changes paddle direction based on what button is pressed
-		if (keyCode == KeyEvent.VK_DOWN) 
+			if (keyCode == KeyEvent.VK_DOWN)
 			// fill this in
-			if (multiplayerCheck) {
-				down2 = true;
-			}
+			down2 = true;
 		
-		if (keyCode == KeyEvent.VK_UP) 
+			if (keyCode == KeyEvent.VK_UP)
 			// fill this in
-			if (multiplayerCheck) {
 				up2 = true;
-			}
 
 		if (e.getKeyChar() == 'w')
 			// move paddle down
@@ -149,8 +235,16 @@ public class Pong extends JPanel implements KeyListener {
 		if (e.getKeyChar() =='s')
 			// fill this in
 			down1 = true;
+		
+		if (e.getKeyChar() == 'p')
+			pause = !pause;
+		
+		if (e.getKeyChar() == 'o')
+			if (rocket2) {
+			rl2 = true;
+			}
 			
-/*		// turn 1-player mode on
+		// turn 1-player mode on
 		if (e.getKeyChar() == '1')
 			// fill this in
 			multiplayerCheck = false;
@@ -160,7 +254,7 @@ public class Pong extends JPanel implements KeyListener {
 		// turn 2-player mode on
 		if (e.getKeyChar() == '2')
 			// fill this in
-			multiplayerCheck = true; */
+			multiplayerCheck = true; 
 	}
 
 	// defines what we want to happen if a keyboard button
@@ -187,6 +281,7 @@ public class Pong extends JPanel implements KeyListener {
 			down1 = false;
 	}
 	
+	
 	// restarts the game, including scores
 	public void restart() {
 
@@ -207,14 +302,20 @@ public class Pong extends JPanel implements KeyListener {
 		while (true) {
 	
 			// draws the game
+			
 			repaint();
 			
 			// we move the ball, paddle, and check for collisions
 			// every hundredth of a second
-			move_ball();
-			move_paddles();
-			check_collisions();
-			
+			if (!pause) {
+				move_ball();
+				move_paddles();
+				check_collisions();
+				
+				// keeps track of time
+				s++;
+				time = s/100;
+			}
 			//rests for a hundredth of a second
 			try {
 				Thread.sleep(10);
